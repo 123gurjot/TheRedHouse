fe3e2c1e65304ddbd5bc1cfd79cfbb24244aa0c9
123gurjot <152326027+123gurjot@users.noreply.github.com>
2024-10-09 18:18:22 +0100

    script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,17 @@ public class Player : MonoBehaviour
 {
     public float Speed;
     private Vector2 Move;
-
+    private Vector2 change;
+    public bool Attacking = false;
+    public bool movement = false;
     public void onMove(InputAction.CallbackContext context)
     {
+        change = Move;
         Move = context.ReadValue<Vector2>();
+    }
+    public void Attack(InputAction.CallbackContext context)
+    {
+        Attacking = context.ReadValue<bool>();
     }
     void Start()
     {
@@ -19,13 +26,28 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-       MovePlayer();
+        MovePlayer();
+        if (Attacking == true)
+        {
+            //for audio
+        }
+        if (change != Move)
+        {
+            movement = true;
+        }
+        if (movement == true) 
+        { 
+            // for audio
+        }
+
     }
 
     public void MovePlayer()
     {
+        
         Vector3 Movement = new Vector3(Move.x, 0f, Move.y);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Movement), 1f);
         transform.Translate(Movement * Speed *  Time.deltaTime, Space.World);
     }
+    
 }
